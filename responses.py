ff871b72b1f1c90576f4763391ac975312efc3a2
day <lilitmousheghyan@gmail.com>
2023-02-05 20:02:20 +0200

    added ai

--- responses.py
+++ responses.py
@@ -1,16 +1,20 @@
 import random
+import openai
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+openai.api_key = config["OPENAI_KEY"]
 
 def handle_response(message):
-    if message == 'hello':
-        return 'sup'
-    
-    if message == 'roll':
-        return str(random.randint(1, 4))
-    
-    if message == 'help':
-        return '`This is ur help`'
-    
-    if message == 'buniii':
-        return 'beeee'
+    response = openai.Completion.create(
+      model = "text-davinci-003",
+      prompt = message,
+      temperature = 0.9,
+      max_tokens = 1024,
+      top_p = 1,
+      frequency_penalty = 0,
+      presence_penalty = 0.6,
+      stop=[" Human:", " AI:"]
+    )
 
-    return "fuk off"
+    return response.choices[0].text
